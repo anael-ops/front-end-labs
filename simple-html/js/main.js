700c910c057f8445071e1666377eaefb08bf2e63
anael-ops <anaclaraajj777@gmail.com>
2025-05-08 22:21:58 -0300

    java script

--- simple-html/js/main.js
+++ simple-html/js/main.js
@@ -1,2 +1,27 @@
-let nome = "Bruno Brother"
-alert("Boa noite," + nome);
+const calc = (a, b, operacao) => {
+    return operacao === "+" ? a + b
+        : operacao === "-" ? a - b
+        : operacao === "*" ? a * b
+        : operacao === "/" ? a / b
+        : "Operação inválida";
+};
+
+const calcCase = (a, b, operacao) => {
+    if (b === 0 && operacao === "/"){
+        return "Não há divisão por zero!";
+    }
+    else{
+    switch(operacao) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            return a / b;
+        default:
+            return "Operação Inválida";
+        }
+    }
+};
