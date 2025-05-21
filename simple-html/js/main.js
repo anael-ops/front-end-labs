be001234548bbb5386e6d8ce47f97b2a0dcb29e4
anael-ops <anaclaraajj777@gmail.com>
2025-05-20 22:27:10 -0300

    Introducao_DOM

--- simple-html/js/main.js
+++ simple-html/js/main.js
@@ -1,27 +1,9 @@
-const calc = (a, b, operacao) => {
-    return operacao === "+" ? a + b
-        : operacao === "-" ? a - b
-        : operacao === "*" ? a * b
-        : operacao === "/" ? a / b
-        : "Operação inválida";
-};
+const linkExterno = document.querySelector('a[title="Google Maps"]');
 
-const calcCase = (a, b, operacao) => {
-    if (b === 0 && operacao === "/"){
-        return "Não há divisão por zero!";
-    }
-    else{
-    switch(operacao) {
-        case "+":
-            return a + b;
-        case "-":
-            return a - b;
-        case "*":
-            return a * b;
-        case "/":
-            return a / b;
-        default:
-            return "Operação Inválida";
-        }
-    }
-};
+//linkExterno.onclick = () => {
+    //  alert("Você será redirecionado para um link externo.");
+//};
+
+linkExterno.addEventListener('click', () => {
+    alert("Você será redirecionado para um link externo.");
+});
